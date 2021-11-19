ce8f0aa991f718addb887f6de15f4d9d4d546048
son <sondevelopgit@gmail.com>
2021-11-19 19:40:43 +0900

    crud 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { v1 as uuid } from 'uuid';
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,8 @@ export class BoardsService {
         return this.boards;
     }
 
-    public createBoard(title: string, description: string) {
+    public createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
         const board: Board = {
             id: uuid(),
             title,
@@ -18,5 +20,20 @@ export class BoardsService {
             status: BoardStatus.PUBLIC,
         };
         this.boards.push(board);
+        return board;
+    }
+
+    public getBoardById(id: string): Board {
+        return this.boards.find((board) => board.id == id);
+    }
+
+    public deleteBoard(id: string): void {
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+    public updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
     }
 }
